aae14df9885a7837a196bee89886b111d03276f7
Noctisdc <camijavier@hotmail.com>
2026-08-20 10:08:27 -0500

    Fix syntax error in image caption display

--- app.py
+++ app.py
@@ -6,6 +6,6 @@ st.title("Welcome to Japan")
 st.header("En este espacio empieza tu viaje por japon")
 st.write("facil mente puedes empezar por tokyo")
 image = Image.open("images (1).jpg")
-st.image(image, caption = ("TOKYO")
+st.image(image, caption = "TOKYO")
 
          
